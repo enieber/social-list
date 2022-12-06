6ff91c0778d0ebb8053de2c94bd85edca1aa249d
enieber cunha <eniebercunha@gmail.com>
2022-12-06 01:29:00 -0300

    tag: fist commit with create user

---
+++ pages/api/account.ts
@@ -0,0 +1,36 @@
+import {
+  NextApiRequest,
+  NextApiResponse,
+} from 'next'
+import bcrypt from 'bcrypt';
+
+import prisma from '../../lib/prisma';
+
+const saltRounds = 10;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const {
+      email,
+      name,
+      password,
+    } = req.body
+    if (!email || !name || !password) {
+      return res.status(400).json({
+        message: 'need params in request'
+      });
+    }
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: passwordHash,
+    },
+  })
+       return res.status(200).json(user);
+  } else {
+    res.status(200).json({ name: 'John Doe' })    
+  }
+}
+
