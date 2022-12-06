4bc245ee815081b9c9c339f070c5bc9b45fa742f
enieber cunha <eniebercunha@gmail.com>
2022-12-06 03:27:47 -0300

    feat: create sigin with generate new token

--- pages/api/sigin.ts
+++ pages/api/sigin.ts
@@ -3,7 +3,8 @@ import {
   NextApiResponse,
 } from 'next'
 import bcrypt from 'bcrypt';
-
+import * as jose from 'jose'
+import fs from 'fs'
 import prisma from '../../lib/prisma';
 
 const saltRounds = 10;
@@ -28,17 +29,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (user) {
       const metchPassword = await bcrypt.compare(password, user.password)
       if (metchPassword) {
+        const alg = "RS256"
+        const pem = fs.readFileSync("./rsakey.pem", "utf8");
+        const privateKey = await jose.importPKCS8(pem, alg)
+
+        const jwt = await new jose.SignJWT({
+          "urn:supplist:claim": true 
+        })
+        .setProtectedHeader({ alg })
+        .setIssuedAt()
+        .setIssuer('urn:supplist:api')
+        .setAudience('urn:supplist:app')
+        .setExpirationTime('2h')
+        .setSubject(user.id)
+        .sign(privateKey)
+
+
+
       return res.status(200).json({
-        token: 'new-token'
+        token: jwt
       });   
       
       }
     }
-    return res.status(400).json({
+    return res.status(401).json({
       message: 'invalid email or password'
     })
-  } else {
-    res.status(405).json({ message: 'method not allowed' })    
   }
+   return res.status(405).json({ message: 'method not allowed' })    
+  
 }
 
